9bc04a3328765ab018a6dee560492a8f52937abf
Tomas Daugela <todaprojects@gmail.com>
2020-12-24 11:52:12 +0200

    HttpResponse class is not static and has a field of dependency

---
+++ WebScraper-ConsoleApp/Utils/HttpResponse.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebScraper_ConsoleApp.Utils
+{
+    public class HttpResponse
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpResponse()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> ParseAsync(string sourceUri)
+        {
+            var response = await _httpClient.GetAsync(sourceUri);
+            if (!response.IsSuccessStatusCode) throw new Exception();
+            
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
